bec678f543302eb42d22bb4ba07bc5fd7a0522c8
XoDeR <xoder.vs@gmail.com>
2025-10-06 19:33:53 +0200

    CRUD API

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Server.Data;
+using Server.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,5 +23,42 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 // API
+app.MapGet("/api/products", async (AppDbContext db) =>
+    await db.Products.ToListAsync());
+
+app.MapGet("/api/products/{id:int}", async (int id, AppDbContext db) =>
+    await db.Products.FindAsync(id) is Product product
+        ? Results.Ok(product)
+        : Results.NotFound());
+
+app.MapPost("/api/products", async (Product product, AppDbContext db) =>
+{
+    db.Products.Add(product);
+    await db.SaveChangesAsync();
+    return Results.Created($"/api/products/{product.Id}", product);
+});
+
+app.MapPut("/api/products/{id:int}", async (int id, Product updatedProduct, AppDbContext db) =>
+{
+    var product = await db.Products.FindAsync(id);
+    if (product is null) return Results.NotFound();
+
+    product.Name = updatedProduct.Name;
+    product.Description = updatedProduct.Description;
+    product.Price = updatedProduct.Price;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/api/products/{id:int}", async (int id, AppDbContext db) =>
+{
+    var product = await db.Products.FindAsync(id);
+    if (product is null) return Results.NotFound();
+
+    db.Products.Remove(product);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
 
 app.Run();
